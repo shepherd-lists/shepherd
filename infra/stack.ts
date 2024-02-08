0b17065fb18375aaafbd6803367c4662f1be7258
Ros McMahon <rosmcmahon@gmail.com>
2024-02-08 12:40:29 +0700

    feat/infra: import shepherd params

--- infra/stack.ts
+++ infra/stack.ts
@@ -1,5 +1,6 @@
-import { App, Stack } from 'aws-cdk-lib'
+import { App, Stack, aws_ec2, aws_ecs, aws_logs } from 'aws-cdk-lib'
 import { Config } from '../../../Config'
+import { GetParameterCommand, SSMClient } from '@aws-sdk/client-ssm'
 
 
 export const createStack = async (app: App, config: Config) => {
@@ -12,6 +13,26 @@ export const createStack = async (app: App, config: Config) => {
 		description: 'indexer run as a private addon'
 	})
 
+	/** initialise imports */
 
+	/** import params */
+	const readParam = async (name: string) => {
+		const ssm = new SSMClient()
+		return (await ssm.send(new GetParameterCommand({
+			Name: `/shepherd/${name}`,
+			WithDecryption: true, // ignored if unencrypted
+		}))).Parameter!.Value as string // throw when undefined
+	}
+	const vpcName = await readParam('VpcName')
+	const RdsEndpoint = await readParam('RdsEndpoint')
+	const logGroupName = await readParam('LogGroup')
+	const clusterName = await readParam('ClusterName')
+
+	/** import shepherd infra */
+	const vpc = aws_ec2.Vpc.fromLookup(stack, 'shepherd-vpc', { vpcName })
+	const logGroup = aws_logs.LogGroup.fromLogGroupName(stack, 'services-logs', logGroupName)
+	const cluster = aws_ecs.Cluster.fromClusterAttributes(stack, 'shepherd-cluster', { vpc, clusterName })
+
+	
 
 }
