c1e0470e2b825f8270561dbd760454df1b52dc55
Ros McMahon <rosmcmahon@gmail.com>
2024-02-09 10:25:06 +0700

    feat: knex config for creating migrations

---
+++ services/indexer-next/knexfile.ts
@@ -0,0 +1,18 @@
+
+module.exports = {
+	client: 'pg',
+	connection: {
+		host: process.env.DB_HOST,
+		port: 5432,
+		database: 'arblacklist',
+		user: 'postgres',
+		password: 'postgres'
+	},
+	pool: {
+		min: 0,
+		max: 10
+	},
+	migrations: {
+		tableName: 'knex_migrations_wallets'
+	}
+}
