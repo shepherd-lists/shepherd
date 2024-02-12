70b44d46fac8f5cc180e40b457adc7581411e0d4
Ros McMahon <rosmcmahon@gmail.com>
2024-02-12 13:31:11 +0700

    fix/fnOwner: timeout slack notifications after 20s no connection

--- lambdas/fnOwnerTable/utils/slackLog.ts
+++ lambdas/fnOwnerTable/utils/slackLog.ts
@@ -4,20 +4,23 @@ console.log('SLACK_WEBHOOK', hookUrl)
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const slackLog = async (...args: any) => {
 
-	const text = '[safer]: ' + args.join(' ')
+	const text = '[fnOwner]: ' + args.join(' ')
 
 	/** log message either way */
 	console.log(slackLog.name, text)
 
-	if(!hookUrl) return console.log(slackLog.name, 'no SLACK_WEBHOOK url provided.')
+	if (!hookUrl) return console.log(slackLog.name, 'no SLACK_WEBHOOK url provided.')
 
-	try{
-		const res = await fetch(hookUrl, { method: 'POST', body: JSON.stringify({ text }) })
+	/** post to slack */
+	const controller = new AbortController()
+	const timeoutId = setTimeout(() => controller.abort(), 20_000)
+	try {
+		const res = await fetch(hookUrl, { method: 'POST', body: JSON.stringify({ text }), signal: controller.signal })
 
-		if(!res.ok) throw new Error(`response not ok: ${res.status}, ${res.statusText}}`)
+		if (!res.ok) throw new Error(`response not ok: ${res.status}, ${res.statusText}}`)
 
 		console.log(slackLog.name, `response: ${res.status}, ${await res.text()}`) // use up and close connnection
-	}catch(e){
+	} catch (e) {
 		console.log(slackLog.name, 'error occurred when posting.', e)
 	}
 }
