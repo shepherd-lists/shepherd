e1df459b448ad3173f911b0c23485ec98d510991
Ros McMahon <rosmcmahon@gmail.com>
2024-02-12 13:07:06 +0700

    feat/fnOwner: slack notifications

---
+++ lambdas/fnOwnerTable/utils/slackLog.ts
@@ -0,0 +1,23 @@
+const hookUrl = process.env.SLACK_WEBHOOK as string
+console.log('SLACK_WEBHOOK', hookUrl)
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const slackLog = async (...args: any) => {
+
+	const text = '[safer]: ' + args.join(' ')
+
+	/** log message either way */
+	console.log(slackLog.name, text)
+
+	if(!hookUrl) return console.log(slackLog.name, 'no SLACK_WEBHOOK url provided.')
+
+	try{
+		const res = await fetch(hookUrl, { method: 'POST', body: JSON.stringify({ text }) })
+
+		if(!res.ok) throw new Error(`response not ok: ${res.status}, ${res.statusText}}`)
+
+		console.log(slackLog.name, `response: ${res.status}, ${await res.text()}`) // use up and close connnection
+	}catch(e){
+		console.log(slackLog.name, 'error occurred when posting.', e)
+	}
+}
