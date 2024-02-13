6724bf53c9ce5c8415bc4ed6308d346d6c631971
Ros McMahon <rosmcmahon@gmail.com>
2024-02-13 23:28:19 +0700

    feat/service: test lambda invoking

--- services/indexer-next/src/index.ts
+++ services/indexer-next/src/index.ts
@@ -1,11 +1,40 @@
 import { slackLog } from './utils/slackLog'
+import { createOwnerTable } from './utils/owner-table-utils'
+import { LambdaClient, InvokeCommand } from '@aws-sdk/client-lambda'
+
 
 const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 
+const lambdaClient = new LambdaClient({})
+
+
 /** restart on errors */
+let runonce = true
 while (true) {
 	try {
 
+		if (runonce) {
+			console.info('create owner table.')
+			const owner = 'v2XXwq_FvVqH2KR4p_x8H-SQ7rDwZBbykSv-59__Avc'
+			const tablename = await createOwnerTable(owner)
+			console.info('owner table created.')
+
+			console.info("let's start a lambda")
+			//import lambda name
+			const fnOwnerTable = process.env.FN_OWNER_TABLE as string
+			console.info('fnOwnerTable', fnOwnerTable)
+			const res = await lambdaClient.send(new InvokeCommand({
+				FunctionName: fnOwnerTable,
+				Payload: JSON.stringify({ owner, tablename }),
+				// InvocationType: 'RequestResponse'
+			}))
+			console.info('res', res)
+			runonce = false
+		}
+
+
+		console.info('nothing to do. ssleeping for 50 seconds...')
+		await new Promise(resolve => setTimeout(resolve, 50_000))
 
 
 	} catch (err: unknown) {
@@ -13,8 +42,8 @@ while (true) {
 		slackLog(
 			`Fatal error occurred: ${e.name}:${e.message}\n`,
 			JSON.stringify(e, null, 2),
-			'\nrestarting in 10 seconds...'
+			'\nrestarting in 30 seconds...'
 		)
-		await sleep(10_000)
+		await sleep(30_000)
 	}
 }
