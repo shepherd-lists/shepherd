37006a7b7e4667e0e5763fd2886314b1a5dfe622
Ros McMahon <rosmcmahon@gmail.com>
2024-02-13 23:37:45 +0700

    feat/fnOwner: try/catch handler => send slackLog

--- lambdas/fnOwnerTable/index.ts
+++ lambdas/fnOwnerTable/index.ts
@@ -50,86 +50,92 @@ const getParent = moize(
 )
 
 export const handler = async (event: any) => {
-	console.log('event', event)
-	const inputs = event as { owner: string, tablename: string }
-	if (!inputs.owner || !inputs.tablename) {
-		throw new Error('missing inputs. should have { "owner": "string", "tablename": "string" }')
-	}
+	try {
+		console.log('event', event)
+		const inputs = event as { owner: string, tablename: string }
+		if (!inputs.owner || !inputs.tablename) {
+			throw new Error('missing inputs. should have { "owner": "string", "tablename": "string" }')
+		}
 
-	/** plan:
-	 * receive tableName & owner
-	 * gql all txids
-	 * calculate ranges
-	 * batch inserts to db, gql page at a time
-	 */
-	const records: OwnerTableRecord[] = []
-	const variables = {
-		owner: inputs.owner,
-		// cursor: '',
-	}
-	const counts = { page: 0, items: 0, inserts: 0 }
-	await gql.all(query, variables, async (page) => {
-		console.info('processing page', ++counts.page)
-		await Promise.all(page.map(async ({ node }) => {
-			counts.items++
+		/** plan:
+		 * receive tableName & owner
+		 * gql all txids
+		 * calculate ranges
+		 * batch inserts to db, gql page at a time
+		 */
+		const records: OwnerTableRecord[] = []
+		const variables = {
+			owner: inputs.owner,
+			// cursor: '',
+		}
+		const counts = { page: 0, items: 0, inserts: 0 }
+		await gql.all(query, variables, async (page) => {
+			console.info('processing page', ++counts.page)
+			await Promise.all(page.map(async ({ node }) => {
+				counts.items++
 
-			/** skip small files */
-			if (+node.data.size < 1_000) {
-				console.log(`file too small, size: ${node.data.size}, txid: ${node.id}`)
-				return;
-			}
+				/** skip small files */
+				if (+node.data.size < 1_000) {
+					console.log(`file too small, size: ${node.data.size}, txid: ${node.id}`)
+					return;
+				}
 
-			/** build records */
-			const txid = node.id
-			const parent = node.parent?.id || null
-			let parents: string[] | undefined = []
+				/** build records */
+				const txid = node.id
+				const parent = node.parent?.id || null
+				let parents: string[] | undefined = []
 
-			// loop to find all nested parents
-			if (parent) {
-				let p: string | null = parent
-				do {
-					const p0: string = p
+				// loop to find all nested parents
+				if (parent) {
+					let p: string | null = parent
+					do {
+						const p0: string = p
 
-					try {
-						p = await getParent(p0, gql)
-					} catch (eOuter: unknown) {
-						console.error(`getParent error: "${(eOuter as Error).message}" while fetching parent: "${p}" for dataItem: ${txid} using gqlProvider: ${gql.endpointUrl}. Trying gqlBackup now.`)
 						try {
-							p = await getParent(p0, gqlBackup)
-						} catch (eInner: unknown) {
-							slackLog(`getParent error: "${(eInner as Error).message}" while fetching parent: ${p0} for dataItem: ${txid} Tried both gql endpoints.`)
+							p = await getParent(p0, gql)
+						} catch (eOuter: unknown) {
+							console.error(`getParent error: "${(eOuter as Error).message}" while fetching parent: "${p}" for dataItem: ${txid} using gqlProvider: ${gql.endpointUrl}. Trying gqlBackup now.`)
+							try {
+								p = await getParent(p0, gqlBackup)
+							} catch (eInner: unknown) {
+								slackLog(`getParent error: "${(eInner as Error).message}" while fetching parent: ${p0} for dataItem: ${txid} Tried both gql endpoints.`)
+							}
 						}
-					}
-				} while (p && parents.push(p))
-			}
-			parents = parents.length === 0 ? undefined : parents
+					} while (p && parents.push(p))
+				}
+				parents = parents.length === 0 ? undefined : parents
 
-			/** calculate byte-range */
-			const range = await getByteRange(txid, parent, parents)
+				/** calculate byte-range */
+				const range = await getByteRange(txid, parent, parents)
 
-			if (range.start === -1n) {
-				console.error(`Error in range calculation for txid: ${txid}`)
-				return;
-			}
+				if (range.start === -1n) {
+					console.error(`Error in range calculation for txid: ${txid}`)
+					return;
+				}
 
-			/** add to records */
-			counts.inserts++
-			records.push({
-				txid,
-				parent,
-				parents,
-				byte_start: range.start.toString(),
-				byte_end: range.end.toString(),
-			})
-		})) //eo promise.all(map)
+				/** add to records */
+				counts.inserts++
+				records.push({
+					txid,
+					parent,
+					parents,
+					byte_start: range.start.toString(),
+					byte_end: range.end.toString(),
+				})
+			})) //eo promise.all(map)
 
-		// /** batch insert this pages results */
-		const inserted = await batchInsert(records, inputs.tablename)
+			// /** batch insert this pages results */
+			const inserted = await batchInsert(records, inputs.tablename)
 
-	})
+		})
 
-	console.info(`completed processing ${JSON.stringify(counts)}`)
+		console.info(`completed processing ${JSON.stringify(counts)}`)
 
 
-	return event
+		return event
+	} catch (err: unknown) {
+		const e = err as Error
+		await slackLog(`${e.name}:${e.message}`)
+		throw e
+	}
 }
