b6716a14eb649f296b57c85422a29698772950bb
Ros McMahon <rosmcmahon@gmail.com>
2024-02-19 11:55:43 +0700

    feat/service: automatically `knex migrate:latest`

--- services/indexer-next/src/index.ts
+++ services/indexer-next/src/index.ts
@@ -1,8 +1,10 @@
 import { slackLog } from './utils/slackLog'
 import { createInfractionsTable } from './utils/owner-table-utils'
 import { blockOwnerHistory } from './owner-blocking'
+import knexCreate from './utils/knexCreate'
 
 
+const knex = knexCreate()
 
 if (!process.env.FN_OWNER_TABLE) throw new Error('missing env var, FN_OWNER_TABLE')
 
@@ -15,14 +17,31 @@ let runonce = true
 while (true) {
 	try {
 
+		/* knex migrate:latest */
+		const [batchNo, logs] = await knex.migrate.latest({
+			directory: new URL('../migrations/', import.meta.url).pathname,
+			tableName: 'knex_migrations_wallets',
+		})
+		if (logs.length !== 0) {
+			console.info('migrate >>', 'Database upgrades complete', batchNo, logs)
+			console.info('migrate >>', 'now running vacuum...')
+			await knex.raw('vacuum verbose analyze;')
+			const vacResults = await knex.raw('SELECT relname, last_vacuum, last_autovacuum FROM pg_stat_user_tables;')
+			for (const row of vacResults.rows) {
+				console.info('migrate >> vacuum results:', JSON.stringify(row))
+			}
+		} else {
+			console.info('migrate >>', 'Database upgrade not required', batchNo, logs)
+		}
+
 		if (runonce) {
-			console.info('create infractions table.')
 			const owner = 'v2XXwq_FvVqH2KR4p_x8H-SQ7rDwZBbykSv-59__Avc'
+
+			// console.info('create infractions table.')
 			// const infractionsTable = await createInfractionsTable(owner)
-			console.info('infractions table created.')
+			// console.info('infractions table created.')
 
 			console.info('run block owner history.')
-
 			await blockOwnerHistory(owner)
 
 			runonce = false
