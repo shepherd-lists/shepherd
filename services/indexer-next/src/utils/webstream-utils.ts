81004945ffbcf0841a37f64b4363aa565ef17af8
Ros McMahon <rosmcmahon@gmail.com>
2024-02-23 15:02:11 +0700

    task/services: combine webstream utils

--- services/indexer-next/src/utils/webstream-utils.ts
+++ services/indexer-next/src/utils/webstream-utils.ts
@@ -1,5 +1,22 @@
-import { iteratorWeb } from './webstream-iterator.js'
+/** 
+ * web streams in the browser dont have an iterator yet. 
+*/
+export async function* iteratorWeb<T>(stream: ReadableStream<T>) {
+	const reader = stream.getReader() //lock 
+	try {
+		while (true) {
+			const { done, value } = await reader.read()
+			if (done) return;
+			yield value as T;
+		}
+	} finally {
+		reader.releaseLock() //unlock
+	}
+}
 
+/**
+ * returns non-empty lines from a stream. 
+ */
 export async function* readlineWeb(stream: ReadableStream<ArrayBuffer>) {
 	const textStream = stream.pipeThrough(new TextDecoderStream)
 
