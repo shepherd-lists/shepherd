6e9eb9b1c087024c06888836fc990c975c78bbe9
Ros McMahon <rosmcmahon@gmail.com>
2024-02-27 12:21:29 +0700

    task/libs: update pgCreate to include batchInserts

--- libs/constants.ts
+++ libs/constants.ts
@@ -3,3 +3,5 @@ export const infraction_limit = 2;
 
 export const network_EXXX_codes = ['ETIMEDOUT', 'ECONNRESET', 'ECONNREFUSED', 'ENOTFOUND', 'EAI_AGAIN']
 
+export const HOST_URL = 'https://arweave.net'
+
