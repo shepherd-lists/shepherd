1f3b4cdb1cc0222c5d23428be6e4b8402e7d28da
Ros McMahon <rosmcmahon@gmail.com>
2024-03-05 12:36:20 +0700

    feat/http-api: new flagged module to handle db table set ups, infraction records/counts,  and s3 list updates

--- services/http-api/src/flagged.ts
+++ services/http-api/src/flagged.ts
@@ -1,6 +1,10 @@
 import { TxRecord } from 'shepherd-plugin-interfaces/types'
 import dbConnection from '../../../libs/utils/knexCreate'
 import { slackLog } from '../../../libs/utils/slackLog'
+import { createInfractionsTable, ownerToInfractionsTablename, ownerToOwnerTablename } from '../../../libs/block-owner/owner-table-utils'
+import { infraction_limit } from '../../../libs/constants'
+import { blockOwnerHistory } from '../../../libs/block-owner/owner-blocking'
+import { updateAddresses, updateFullTxidsRanges } from '../../../libs/s3-lists/update-lists'
 
 const knex = dbConnection()
 
@@ -17,11 +21,11 @@ export const processFlagged = async (
 	 * - update tx in db
 	 * -- remove from inbox/inflights
 	 * -- insert to txs
-	 * - update s3 with query
 	 * 2. owner update
 	 * - update owners_list
 	 * - schedule blocking if necessary
-	 */
+	 * 3. update s3
+	*/
 
 	const trx = await knex.transaction()
 	try {
@@ -43,16 +47,55 @@ export const processFlagged = async (
 			.returning('txid')
 		const insertedId = resInsert[0]?.txid
 		if (insertedId !== txid) {
-			await slackLogger(txid, 'ERROR ❌ cannot insert flagged to txs', `(${JSON.stringify(updates)}) => ${resInsert}`)
+			await slackLog(txid, 'ERROR ❌ cannot insert flagged to txs', `(${JSON.stringify(updates)}) => ${resInsert}`)
+			throw new Error(`Could not insert ${txid} into txs`) //cause a rollback
 		}
+
 		/** 2. owner update */
 		/** update owners_list */
+		const owner = record.owner!
+		const tablename = ownerToOwnerTablename(owner)
+		const infractionsTablename = ownerToInfractionsTablename(owner)
+		const ownerRecord = await trx('owners_list').where('owner', owner).first()
+
+		let infractions = 0
+		if (ownerRecord) {
+			infractions = ownerRecord.infractions
+		} else {
+			await createInfractionsTable(owner)
+		}
+		const alreadyExists = await knex(infractionsTablename).where('txid', txid).first()
+		if (!alreadyExists) {
+			infractions++
+			await knex(infractionsTablename).insert({ txid })
+		} else {
+			await slackLog(txid, 'ERROR ❌ already exists in infractions', `(${JSON.stringify(updates)}) => ${resInsert}`)
+			throw new Error(`Already exists in infractions`) //cause a rollback
+		}
+
+		if (ownerRecord) {
+			await trx('owners_list').where('owner', owner).update({ infractions })
+		} else {
+			await trx('owners_list').insert({ owner, infractions, add_method: 'auto' })
+		}
+
 		/** schedule blocking if necessary */
+		if (infractions > infraction_limit) {
+			blockOwnerHistory(owner) // cannot rollback!
+
+			/** update s3://addresses.txt */
+			await updateAddresses()
+		}
+
+		/** update s3-lists. this causes a full re-write. use sparingly */
+		await updateFullTxidsRanges()
+
+
 
 		await trx.commit()
 	} catch (e) {
 		trx.rollback()
-		throw e
+		throw e // will this cause client to retry?
 	}
 
 }
