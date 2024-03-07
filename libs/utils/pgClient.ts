ac61cfb737dac4adcd96a1d16042ca9af600162e
Ros McMahon <rosmcmahon@gmail.com>
2024-03-07 17:47:17 +0700

    task: code & log cleanups

--- libs/utils/pgClient.ts
+++ libs/utils/pgClient.ts
@@ -66,8 +66,3 @@ export const batchInsert = async <T extends object>(records: T[], tableName: str
 		throw e
 	}
 }
-
-/** we can't use `-` in postgres table names, and usual starting character rules */
-export const ownerToTablename = (owner: string) => `owner_${owner.replace(/-/g, '~')}`
-export const tablenameToOwner = (tablename: string) => tablename.slice('owner_'.length).replace(/~/g, '-')
-
