206784825163249b98b941fb3c32a6d05ebf5f2a
Ros McMahon <rosmcmahon@gmail.com>
2024-03-15 20:09:31 +0700

    task/checkBlocked: refactor cached blocked ranges module

--- services/webserver-next/src/checkBlocking2/ranges-cachedBlocked.ts
+++ services/webserver-next/src/checkBlocking2/ranges-cachedBlocked.ts
@@ -16,19 +16,19 @@ interface RangeCache {
 }
 const _rangeCache: RangeCache = { eTag: '', ranges: [], inProgress: false }
 
-export const blockedRanges = async () => {
+export const getBlockedRanges = async () => {
 	const eTag = (await s3HeadObject(process.env.LISTS_BUCKET!, 'rangelist.txt')).ETag!
-	console.debug(blockedRanges.name, 'eTag', eTag)
+	console.debug(getBlockedRanges.name, 'eTag', eTag)
 
 	/** short-circuit */
 	if (eTag === _rangeCache.eTag) {
-		console.info(blockedRanges.name, 'returning cache')
+		console.info(getBlockedRanges.name, 'returning cache')
 		return _rangeCache.ranges
 	}
 
 	/** overload check: just one update is allowed/required */
 	if (_rangeCache.inProgress) {
-		slackLog(blockedRanges.name, 'WARNING! waiting for cache update as inProgress. indicates server overloaded!')
+		slackLog(getBlockedRanges.name, 'WARNING! waiting for cache update as inProgress. indicates server overloaded!')
 		while (_rangeCache.inProgress) {
 			await sleep(500) //wait for new cache
 		}
@@ -38,7 +38,7 @@ export const blockedRanges = async () => {
 
 	/** fetch rangelist.txt */
 
-	console.info(blockedRanges.name, 'fetching & processing new cache...')
+	console.info(getBlockedRanges.name, 'fetching & processing new cache...')
 	const t0 = performance.now()
 
 	const stream = await s3GetObjectWebStream(process.env.LISTS_BUCKET!, 'rangelist.txt')
@@ -46,14 +46,14 @@ export const blockedRanges = async () => {
 	for await (const line of readlineWeb(stream)) {
 		//DEBUG
 		if (line.length === 0) {
-			slackLog(blockedRanges.name, 'WARNING! empty line retrieving rangelist.txt')
+			slackLog(getBlockedRanges.name, 'WARNING! empty line retrieving rangelist.txt')
 			continue;
 		}
 		const [start, end] = line.split(',').map(Number) as ByteRange
 		ranges.push([start, end])
 	}
 	const t1 = performance.now()
-	console.info(blockedRanges.name, `fetched ${ranges.length} ranges in ${(t1 - t0).toFixed(0)}ms`)
+	console.info(getBlockedRanges.name, `fetched ${ranges.length} ranges in ${(t1 - t0).toFixed(0)}ms`)
 
 	/** merge contiguous ranges */
 
@@ -83,11 +83,11 @@ export const blockedRanges = async () => {
 		})
 	}
 	const t2 = performance.now()
-	console.info(blockedRanges.name, `merged ${mergedRanges.length} ranges in ${(t2 - t1).toFixed(0)}ms.`)
+	console.info(getBlockedRanges.name, `merged ${mergedRanges.length} ranges in ${(t2 - t1).toFixed(0)}ms.`)
 
 	/** finish up */
 
-	console.info(blockedRanges.name, `Total time: ${(t2 - t0).toFixed(0)}ms`)
+	console.info(getBlockedRanges.name, `Total time: ${(t2 - t0).toFixed(0)}ms`)
 	_rangeCache.inProgress = false
 	return _rangeCache.ranges = mergedRanges
 }
