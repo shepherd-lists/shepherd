978a0d482f8c291d1fe410650ed37be5b14df402
Ros McMahon <rosmcmahon@gmail.com>
2024-05-06 20:10:17 +0700

    task/checking/txids: disable txidowners checks (some issue @ 50k requests)

--- services/webserver-next/src/checkBlocking2/index-cron.ts
+++ services/webserver-next/src/checkBlocking2/index-cron.ts
@@ -5,12 +5,12 @@
  * this file contains only the timers
  */
 const FLAGGED_INTERVAL = 30_000 // 30 secs 
-const OWNERS_INTERVAL = 300_000 // 5 mins
+const OWNERS_INTERVAL = 300_000 // 5 mins N.B. owners will be large and take hours to complete
 const DNSR_INTERVAL = 600_000 // 10 mins
 
 
 import { checkFlaggedTxids, checkOwnersTxids } from './index-entry'
 /** main entrypoints */
 setInterval(checkFlaggedTxids, FLAGGED_INTERVAL)
-setInterval(checkOwnersTxids, OWNERS_INTERVAL)
-
+// setInterval(checkOwnersTxids, OWNERS_INTERVAL)
+// checkOwnersTxids()
