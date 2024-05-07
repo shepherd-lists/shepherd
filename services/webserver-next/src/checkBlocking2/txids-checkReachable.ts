4cece58a8cfc87109787a9db79914fce1e141e9e
Ros McMahon <rosmcmahon@gmail.com>
2024-05-07 22:09:36 +0700

    feat/checking/txids: checkUnreachable without http2

---
+++ services/webserver-next/src/checkBlocking2/txids-checkReachable.ts
@@ -0,0 +1,28 @@
+import http from 'http'
+import https from 'https'
+
+
+export const checkReachable = async (url: string) => new Promise<boolean>(resolve => {
+	const protocol = url.startsWith('https') ? https : http
+	const req = protocol.request(url, { method: 'HEAD' },
+		res => {
+			resolve(res.statusCode === 200 ? true : false)
+			res.on('data', () => { }) //must use up empty stream
+		}
+	)
+	req.on('error', () => {
+		resolve(false)
+		req.destroy()
+	})
+	req.setTimeout(2_000, () => {
+		resolve(false)
+		req.destroy() //also calls error but takes some time
+	})
+	req.end()
+})
+
+
+// checkReachable('https://arweave.net').then(console.info)
+// checkReachable('https://arweave.dev').then(console.info)
+// checkReachable('http://1.1.1.1').then(console.info) //returns 30x, so false
+
