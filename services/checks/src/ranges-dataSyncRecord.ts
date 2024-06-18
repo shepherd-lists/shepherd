4adcad1f3ea2679097d7fe95f5c6588ae10d35b9
Ros McMahon <rosmcmahon@gmail.com>
2024-06-18 22:48:14 +0700

    feat/checks/ranges: convert fetch src stream to http2 stream

--- services/checks/src/ranges-dataSyncRecord.ts
+++ services/checks/src/ranges-dataSyncRecord.ts
@@ -1,4 +1,7 @@
-//no imports
+import http2 from 'http2'
+import tls from 'node:tls'
+
+
 
 export async function* iteratorWeb<T>(stream: ReadableStream<T>) {
 	const reader = stream.getReader() //lock
@@ -45,13 +48,86 @@ async function* singleKeyJsonStream(body: ReadableStream<Uint8Array>) {
 	}
 }
 
-
-export const dataSyncObjectStream = async (domain: string, port: number) => {
-	try {
-		const res = await fetch(`http://${domain}:${port}/data_sync_record`, {
-			headers: { 'content-type': 'application/json' },
+/** cannot mix http2 and fetch/undici streams in the same process */
+export const http2ReadableStream = async (url: string, path: string) => {
+	interface RetType {
+		headers: http2.IncomingHttpHeaders & http2.IncomingHttpStatusHeader
+		status?: number
+		ok: boolean
+		body?: ReadableStream
+	}
+	return new Promise<RetType>((resolve, reject) => {
+		const client = http2.connect(url, {
+			rejectUnauthorized: false,
+			/** modifying `tls` options is required for compatibility across various servers */
+			createConnection: (authority, options) => {
+				console.debug({ authority, options })
+				const protocolHttps = authority.protocol === 'https:'
+				/* using host with http connnections makes the socket try to handshake & fail */
+				const host = protocolHttps ? authority.hostname : undefined
+				return tls.connect({
+					...options,
+					host,
+					servername: authority.hostname,
+					port: +authority.port || (protocolHttps ? 443 : 80),
+					ALPNProtocols: ['h2', 'http/1.1'],
+				})
+			}
 		})
-		if (!res.ok) throw new Error(`${dataSyncObjectStream.name} bad response: ${res.status} ${res.statusText}`)
+		const req = client.request({
+			':path': path,
+			'content-type': 'application/json',
+		})
+
+		/** log connection errors */
+		client.on('error', (err) => {
+			console.error('client error:', err)
+			reject(err)
+		})
+		req.on('error', err => {
+			console.error('request connection error:', err)
+			reject(err)
+		})
+
+		req.on('response', (headers, flags) => {
+
+			const readStream = new ReadableStream({
+				start(controller) {
+					req.on('data', (chunk) => {
+						controller.enqueue(chunk)
+					})
+
+					req.on('end', () => {
+						controller.close()
+						client.close()
+					})
+
+					req.on('error', (err) => {
+						console.error('request stream error:', err)
+						controller.error(err)
+						client.close()
+						reject(err)
+					})
+				}
+			})
+
+			resolve({
+				headers,
+				status: headers[':status'],
+				ok: headers[':status'] === 200,
+				body: readStream,
+			})
+		})
+
+		req.end()
+	})
+}
+
+export const dataSyncObjectStream = async (domain: string, port: number, protocol: 'http' | 'https' = 'http') => {
+	try {
+		const res = await http2ReadableStream(`${protocol}://${domain}:${port}`, '/data_sync_record')
+
+		if (!res.ok) throw new Error(`${dataSyncObjectStream.name} bad response: ${res.status}`)
 
 		return singleKeyJsonStream(res.body!)
 	} catch (e) {
@@ -60,7 +136,7 @@ export const dataSyncObjectStream = async (domain: string, port: number) => {
 }
 
 // // Usage example
-// const iter = await dataSyncObjectStream('arweave.net', 80)
+// const iter = await dataSyncObjectStream('arweave.net', 443, 'https')
 // let count = 0
 // for await (const obj of iter) {
 // 	console.debug('obj', obj)
