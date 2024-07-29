9e22c7b85fa2d885605d96231d73ade722365cc4
Ros McMahon <rosmcmahon@gmail.com>
2024-07-29 12:18:49 +0700

    feat/byte-ranges: Config option for http_api_nodes and supporting export functions

--- libs/byte-ranges/txidToRange/constants-byteRange.ts
+++ libs/byte-ranges/txidToRange/constants-byteRange.ts
@@ -1,40 +1,29 @@
+import { Config } from '../../../../../Config'
 /* for use in calculating byte ranges */
 export const CHUNK_ALIGN_GENESIS = 30607159107830n //weave address where enforced 256kb chunks started
 export const CHUNK_SIZE = 262144n //256kb
-
+export const HOST_URL = process.env.HOST_URL!
 export const GQL_URL_SECONDARY = process.env.GQL_URL_SECONDARY!
 export const GQL_URL = process.env.GQL_URL!
+const http_api_nodes = (JSON.parse(process.env.http_api_nodes!) as Config['http_api_nodes']).map(n => ({ ...n, url: `http://${n.name}:1984` }))
+if (!HOST_URL) throw new Error(`Missing env var, HOST_URL:${HOST_URL}`)
 if (!GQL_URL_SECONDARY) throw new Error(`Missing env var, GQL_URL_SECONDARY:${GQL_URL_SECONDARY}`)
 if (!GQL_URL) throw new Error(`Missing env var, GQL_URL:${GQL_URL}`)
+if (!http_api_nodes) throw new Error(`Missing env var, http_api_nodes:${http_api_nodes}`)
 
 
 /** 
- * do some crude gw url switching for test purposes
+ * do some switching and allow fallback
 */
-
-export let HOST_URL = 'https://arweave.net'
-
-const gwUrls: `https://${string}`[] = [
-	'https://arweave.net',
-	// 'https://ar-io.net', //it's arweave.net
-	'https://permagate.io',
-	'https://vilenarios.com',
-]
+export const http_api_nodes_length = http_api_nodes.length
 let index = 0
-const getNext = () => {
+export const nodeApiUrl = (fallback = false) => {
+	if (fallback) return HOST_URL
+
 	index++
-	if (index >= gwUrls.length) {
+	if (index >= http_api_nodes.length) {
 		index = 0
 	}
-	return index
+	return http_api_nodes[index].url
 }
-export const hostUrlRateLimited = async (current: string) => {
-	if (current !== HOST_URL) {
-		//already changed
-		return
-	}
-	const previous = HOST_URL
-	const next = getNext()
-	HOST_URL = gwUrls[next]!
-	console.info(`HOST_URL switching ${previous} => ${HOST_URL}`)
-}
+
