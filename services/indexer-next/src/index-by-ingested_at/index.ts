36e25e342004b394173d48000bfee06f8381500f
Ros McMahon <rosmcmahon@gmail.com>
2024-10-04 23:38:23 +0700

    feat(indexer-ingest): main goldsky query + helper functions

--- services/indexer-next/src/index-by-ingested_at/index.ts
+++ services/indexer-next/src/index-by-ingested_at/index.ts
@@ -1,4 +1,55 @@
+import pool from '../../../../libs/utils/pgClient'
 
 
+const ingestQuery = `
+query($cursor: String, $minAt: Int, $maxAt: Int) {
+	transactions(
+		# your query parameters
 
-export const ingestLoop = async () => { }
+		ingested_at: {
+			min: $minAt,
+			max: $maxAt,
+		}
+		#remove pending results
+		sort: HEIGHT_DESC
+
+		tags: [
+			{ name: "Content-Type", values: ["video/*", "image/*"], match: WILDCARD}
+		]
+		
+		# standard template below
+		after: $cursor
+		first: 100
+	) {
+		pageInfo{hasNextPage}
+		edges {
+			cursor
+			node {
+				# what tx data you want to query for:
+				id
+				id
+				data{ size type }
+				tags{ name value }
+				block{ height }
+				parent{ id }
+				owner{ address }
+			}
+		}
+	}
+}
+`
+
+const indexName = 'indexer_ingest'
+
+const readPositionOrig = async () => (await pool.query(`SELECT value FROM states WHERE pname = 'indexer_ingest'`)).rows[0].value
+
+export const ingestLoop = async (
+	readPosition = readPositionOrig,
+) => {
+
+	const lastMax = await readPosition()
+	console.debug('indexer_ingest position', lastMax)
+
+
+}
+
