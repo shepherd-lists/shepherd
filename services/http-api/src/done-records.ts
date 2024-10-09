8e98228c2e28bd25887dc27c24a8af4ec1be905d
Ros McMahon <rosmcmahon@gmail.com>
2024-10-09 16:40:13 +0700

    task(http-api/moveRecords): cleanup code

--- services/http-api/src/done-records.ts
+++ services/http-api/src/done-records.ts
@@ -1,7 +1,6 @@
-import { StateRecord, TxRecord } from 'shepherd-plugin-interfaces/types'
+import { TxRecord } from 'shepherd-plugin-interfaces/types'
 import dbConnection from '../../../libs/utils/knexCreate'
 import { moveInboxToTxs } from './move-records'
-import moize from 'moize'
 
 const knex = dbConnection()
 
@@ -13,7 +12,6 @@ const inbox2txs = async () => {
 		return
 	}
 	_running = true
-	// const pass2height = await pass2Height()
 	const finished = await knex<TxRecord>('inbox')
 		.select('txid')
 		// .whereNotNull('flagged')
@@ -31,58 +29,6 @@ const inbox2txs = async () => {
 	_running = false
 }
 
+
 setInterval(inbox2txs, 30_000)
 inbox2txs()
-
-/** export for test */
-// export const pass2Height = moize(
-// 	async () => (await knex<StateRecord>('states').where('pname', '=', 'indexer_pass2'))[0]?.value,
-// 	{ maxAge: 30_000, isPromise: true, },
-// )
-
-// export const doneAdd = async (txid: string, height: number) => {
-// 	/** dont add dupes */
-// 	if (done.map(r => r.txid).includes(txid)) {
-// 		console.warn(txid, doneAdd.name, 'warning: not adding duplicate!')
-// 		return done.length
-// 	}
-
-// 	console.info(txid, doneAdd.name, `adding to done. moving: ${moving}, done.length: ${done.length}`)
-// 	done.push({ txid, height })
-
-// 	const now = Date.now()
-// 	const timeDiff = now - last
-// 	if (done.length >= 100 || timeDiff > 30_000) {
-// 		console.info(txid, doneAdd.name, `calling moveDone. done.length: ${done.length}, now - last: ${timeDiff}`)
-// 		await moveDone()
-// 		if (timeDiff > 30_000) last = now
-// 	} else {
-// 		console.info(txid, doneAdd.name, `not moving yet. done.length: ${done.length}, now - last: ${timeDiff}`)
-// 	}
-
-// 	return done.length
-// }
-
-// let moving = false
-// export const moveDone = async () => {
-// 	if (!moving) {
-// 		moving = true
-
-// 		// const movable = done.filter(r => r.height < pass2height)
-
-// 		console.info(moveDone.name, `moving ${done.length} records to txs.`)
-// 		let count = 0
-// 		while (done.length > 0) {
-// 			const moving = done.splice(0, Math.min(100, done.length)).map(r => r.txid)
-// 			count += await moveInboxToTxs(moving)
-// 			done = done.filter(r => !moving.includes(r.txid)) //??
-// 		}
-
-// 		moving = false
-// 		return count
-// 	} else {
-// 		console.info(moveDone.name, 'already moving')
-// 		return
-// 	}
-// }
-
