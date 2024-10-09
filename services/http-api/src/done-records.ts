57626bf6de3de0107398d6d48edf54780d24c0e7
Ros McMahon <rosmcmahon@gmail.com>
2024-10-09 14:13:22 +0700

    fix(http-api/moveRecords): remove dependency on pass2 height - there is no pass2 now.

--- services/http-api/src/done-records.ts
+++ services/http-api/src/done-records.ts
@@ -14,22 +14,22 @@ let last = Date.now()
 
 /** call this early */
 export const doneInit = async () => {
-	const pass2height = await pass2Height()
+	// const pass2height = await pass2Height()
 	const archived = await knex<TxRecord>('inbox')
 		.select('txid', 'height')
 		.whereNotNull('flagged')
 
-	console.info(doneInit.name, `found ${archived.length} records in inbox. pass2.height: ${pass2height}`)
+	console.info(doneInit.name, `found ${archived.length} records in inbox.`)
 
 	//return for test
 	return done = archived
 }
 
 /** export for test */
-export const pass2Height = moize(
-	async () => (await knex<StateRecord>('states').where('pname', '=', 'indexer_pass2'))[0]?.value,
-	{ maxAge: 30_000, isPromise: true, },
-)
+// export const pass2Height = moize(
+// 	async () => (await knex<StateRecord>('states').where('pname', '=', 'indexer_pass2'))[0]?.value,
+// 	{ maxAge: 30_000, isPromise: true, },
+// )
 
 export const doneAdd = async (txid: string, height: number) => {
 	/** dont add dupes */
@@ -43,10 +43,10 @@ export const doneAdd = async (txid: string, height: number) => {
 
 	const now = Date.now()
 	const timeDiff = now - last
-	if (done.length >= 100 || timeDiff > 60_000) {
+	if (done.length >= 100 || timeDiff > 30_000) {
 		console.info(txid, doneAdd.name, `calling moveDone. done.length: ${done.length}, now - last: ${timeDiff}`)
 		await moveDone()
-		if (timeDiff > 60_000) last = now
+		if (timeDiff > 30_000) last = now
 	} else {
 		console.info(txid, doneAdd.name, `not moving yet. done.length: ${done.length}, now - last: ${timeDiff}`)
 	}
@@ -59,15 +59,14 @@ export const moveDone = async () => {
 	if (!moving) {
 		moving = true
 
-		const pass2height = await pass2Height()
-		const movable = done.filter(r => r.height < pass2height)
+		// const movable = done.filter(r => r.height < pass2height)
 
-		console.info(moveDone.name, `moving movable ${movable.length}/${done.length} records to txs. pass2.height: ${pass2height}`)
+		console.info(moveDone.name, `moving ${done.length} records to txs.`)
 		let count = 0
-		while (movable.length > 0) {
-			const moving = movable.splice(0, Math.min(100, movable.length)).map(r => r.txid)
+		while (done.length > 0) {
+			const moving = done.splice(0, Math.min(100, done.length)).map(r => r.txid)
 			count += await moveInboxToTxs(moving)
-			done = done.filter(r => !moving.includes(r.txid))
+			done = done.filter(r => !moving.includes(r.txid)) //??
 		}
 
 		moving = false
