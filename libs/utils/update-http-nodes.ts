c7f35ea4f0c73513e7be5f3d60ab1ee548db0c21
Ros McMahon <rosmcmahon@gmail.com>
2024-11-07 21:58:43 +0700

    feat(libs/update-nodes): also process`rangeItems` inputs + fix & export types

--- libs/utils/update-http-nodes.ts
+++ libs/utils/update-http-nodes.ts
@@ -2,14 +2,13 @@
 
 const http_api_nodes_url = process.env.http_api_nodes_url
 
-type Http_Api_Nodes = Array<{
+export type Http_Api_Node = {
 	name: string //dns name
 	server: string //ip address
 	url?: string // complete node url `http://${name}:1984`
-	throttled?: EpochTimeStamp // used in /offsets
-}>
+}
 /* populate with env var if exists */
-let _nodes = (JSON.parse(process.env.http_api_nodes || '[]') as Http_Api_Nodes).map(n => ({ ...n, url: `http://${n.name}:1984` }))
+let _nodes = (JSON.parse(process.env.http_api_nodes || '[]') as Array<Http_Api_Node>).map(n => ({ ...n, url: `http://${n.name}:1984` }))
 
 /** cron function */
 const checkEndpoint = async () => {
@@ -18,7 +17,7 @@ const checkEndpoint = async () => {
 	}
 
 	try {
-		const nodes: Http_Api_Nodes = await fetch(http_api_nodes_url).then(res => res.json().then(j => j))
+		const nodes: Http_Api_Node[] = await fetch(http_api_nodes_url).then(res => res.json().then(j => j))
 
 		_nodes = nodes.map(n => ({ ...n, url: `http://${n.name}:1984` }))
 		console.debug({ _nodes })
@@ -34,6 +33,15 @@ setInterval(checkEndpoint, interval)
 /** use a cron to check for updates. update on access too complicated/costly for the consumers */
 
 
-/** other modules will only read the updated array */
+/** other modules can just read the updated arrays */
+
+/** for http/offset */
 export const httpApiNodes = () => _nodes
+
+/** range items */
+const rangelistAllowed = JSON.parse(process.env.RANGELIST_ALLOWED || '[]') as Http_Api_Node[]
+export const rangeItems = () => {
+
+	return [..._nodes, rangelistAllowed.filter(ei => !_nodes.find(n => n.name === ei.name))]
+}
 
