8c109cd93bb158ff39f2151dc38fbabb2a8b4ee3
Ros McMahon <rosmcmahon@gmail.com>
2024-11-07 15:36:12 +0700

    feat(libs/httpApiNodes): add `url`s to nodes

--- libs/utils/update-http-nodes.ts
+++ libs/utils/update-http-nodes.ts
@@ -2,8 +2,14 @@
 
 const http_api_nodes_url = process.env.http_api_nodes_url
 
-type Http_Api_Nodes = Array<{ name: string; server: string }>
-let _nodes: Array<Http_Api_Nodes> = JSON.parse(process.env.http_api_nodes || '[]') //populate with env var if exists
+type Http_Api_Nodes = Array<{
+	name: string //dns name
+	server: string //ip address
+	url?: string // complete node url `http://${name}:1984`
+	throttled?: EpochTimeStamp // used in /offsets
+}>
+/* populate with env var if exists */
+let _nodes = (JSON.parse(process.env.http_api_nodes || '[]') as Http_Api_Nodes).map(n => ({ ...n, url: `http://${n.name}:1984` }))
 
 /** cron function */
 const checkEndpoint = async () => {
@@ -12,7 +18,9 @@ const checkEndpoint = async () => {
 	}
 
 	try {
-		_nodes = await fetch(http_api_nodes_url).then(res => res.json().then(j => j))
+		const nodes: Http_Api_Nodes = await fetch(http_api_nodes_url).then(res => res.json().then(j => j))
+
+		_nodes = nodes.map(n => ({ ...n, url: `http://${n.name}:1984` }))
 		console.debug({ _nodes })
 	} catch (e) {
 		console.error(`error fetching http_api_nodes_url=${http_api_nodes_url}`)
@@ -27,4 +35,5 @@ setInterval(checkEndpoint, interval)
 
 
 /** other modules will only read the updated array */
-export let httpApiNodes = () => _nodes
+export const httpApiNodes = () => _nodes
+
