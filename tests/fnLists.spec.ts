ff5c3417d35e20ad141015613de915fc997a4024
Ros McMahon <rosmcmahon@gmail.com>
2024-12-09 22:00:23 +0700

    test(fnList): remove old, useless, potentially damaging test

--- tests/fnLists.spec.ts
+++ tests/fnLists.spec.ts
@@ -10,35 +10,9 @@ import { ByteRange } from '../libs/s3-lists/merge-ranges';
 
 describe('fnLists tests', () => {
 
-	it('should process all lists into s3-lists', async () => {
-		/**
-		 * BE CAREFUL! THIS ACTUALLY RUNS THE HANDLER AND OVERWRITES S3 LISTS
-		 * check .env file
-		 */
-		const event = {
-			test: "testing"
-		}
-		const result = await handler(event)
+	// it('should process all lists into s3-lists', async () => {
+	/**  better just running fnList in aws-console in dev	 */
 
-		assert.ok(result, 'should have a result')
-		assert.ok(result > 0, 'should have a result greater than zero')
-
-		const s3lists = [
-			'txidflagged.txt',
-			'txidowners.txt',
-			'rangelist.txt',
-			'rangeflagged.txt',
-			'rangeowners.txt',
-		]
-		try {
-			for (const Key of s3lists) {
-				await s3HeadObject(process.env.LISTS_BUCKET!, Key)
-			}
-			assert.ok(true, 'should have all s3lists')
-		} catch (e) {
-			assert.fail((e as Error).message)
-		}
-	})
 
 	it(`should test i/o to ${processAddonTable.name} function`, async () => {
 		await pg.query('DROP TABLE IF EXISTS test_txs')
