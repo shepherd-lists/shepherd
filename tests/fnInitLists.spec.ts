da47bddcd5a8d39241ea1534f354e8b4c0dea499
Ros McMahon <rosmcmahon@gmail.com>
2025-04-16 11:17:11 +0100

    test: fix lambda tests

--- tests/fnInitLists.spec.ts
+++ tests/fnInitLists.spec.ts
@@ -2,24 +2,23 @@ import 'dotenv/config'
 import { handler } from '../lambdas/fnInitLists/index'
 import assert from "node:assert/strict";
 import { after, afterEach, beforeEach, describe, it, mock } from 'node:test'
-import { s3HeadObject } from '../libs/utils/s3-services'
+import { s3DeleteFolder, s3HeadObject } from '../libs/utils/s3-services'
 import { processAddonTable } from '../lambdas/fnInitLists/table-processing'
 import pg from '../libs/utils/pgClient'
 import { ByteRange } from '../libs/s3-lists/merge-ranges';
 
 
-describe('fnLists tests', () => {
+describe('fnInitLists tests', () => {
 
 	// it('should process all lists into s3-lists', async () => {
 	/**  better just running fnList in aws-console in dev	 */
 
 
 	it(`should test i/o to ${processAddonTable.name} function`, async () => {
-		await pg.query('DROP TABLE IF EXISTS test_txs')
 		await pg.query('CREATE TABLE test_txs (txid CHAR(43), flagged BOOLEAN, byte_start BIGINT, byte_end BIGINT)')
 		try {
 			const fakeIds = ['fake-id-1', 'fake-id-2'].map(s => s.padEnd(43, '_'))
-			await pg.query('INSERT INTO test_txs (txid, flagged, byte_start, bytes_end) VALUES ($1, true, 1, 2), ($2, false, 3, 4)', fakeIds)
+			await pg.query('INSERT INTO test_txs (txid, flagged, byte_start, byte_end) VALUES ($1, true, 1, 2), ($2, false, 3, 4)', fakeIds)
 			console.debug((await pg.query('select * from test_txs')).rows)
 			const ranges: ByteRange[] = []
 
@@ -28,6 +27,7 @@ describe('fnLists tests', () => {
 				highWaterMark: 200,
 				ranges,
 				LISTS_BUCKET: process.env.LISTS_BUCKET!,
+				postfix: 'postfix/',
 			})
 
 			// console.debug({ ranges })
@@ -39,8 +39,9 @@ describe('fnLists tests', () => {
 	})
 
 	after(async () => {
+		await s3DeleteFolder(process.env.LISTS_BUCKET!, 'test/')
+		await pg.query('DROP TABLE IF EXISTS test_txs')
 		await pg.end()
-		// this is dirty, we leave s3 folder `test/*` behind
 	})
 
 })
