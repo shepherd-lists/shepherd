3eab3d8b1382262b2d4147136d1f593d7c17c340
Ros McMahon <rosmcmahon@gmail.com>
2025-04-18 15:41:27 +0100

    fix(infra/fnOwner): missing aws perimission & env var

--- infra/stack.ts
+++ infra/stack.ts
@@ -70,6 +70,7 @@ export const createStack = async (app: App, config: Config) => {
 			HOST_URL: config.host_url || 'https://arweave.net',
 			http_api_nodes: JSON.stringify(config.http_api_nodes),
 			http_api_nodes_url: config.http_api_nodes_url || '', //byte-ranges
+			LISTS_BUCKET: `shepherd-lists-${config.region}`,
 		},
 	})
 	/** create lambda to process incoming items */
@@ -279,5 +280,6 @@ export const createStack = async (app: App, config: Config) => {
 	listsBucket.grantReadWrite(taskRoleWeb)
 	listsBucket.grantReadWrite(taskRoleHttpApi)
 	listsBucket.grantReadWrite(fnInitLists.role!)
+	listsBucket.grantReadWrite(fnOwnerBlocking.role!)
 
 }
