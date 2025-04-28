4cc0a282dfe363cf08d73f014ab4982dbc101593
Ros McMahon <rosmcmahon@gmail.com>
2025-04-29 00:55:07 +0100

    test(libs): mutex tests

---
+++ tests/lib-util-mutex.spec.ts
@@ -0,0 +1,35 @@
+import assert from "node:assert/strict"
+import { after, afterEach, beforeEach, describe, it } from 'node:test'
+import { createMutex } from '../libs/utils/mutex'
+
+
+describe('mutex', () => {
+	it('should allow only one task to run at a time from each scope', async () => {
+		const mutex1 = createMutex()
+		const mutexA = createMutex()
+		const completed1: string[] = []
+		const completedA: string[] = []
+
+		const tasks1 = ['1', '2', '3'].map(id => mutex1.runExclusive(async () => {
+			// console.log(`Task ${id} acquired lock`)
+			await new Promise((resolve) => setTimeout(resolve, 100))
+			completed1.push(id)
+			// console.log(`Task ${id} releasing lock`)
+		}))
+
+		const tasksA = ['A', 'B', 'C'].map(id => mutexA.runExclusive(async () => {
+			// console.log(`Task ${id} acquired lock`)
+			await new Promise((resolve) => setTimeout(resolve, 100))
+			completedA.push(id)
+			// console.log(`Task ${id} releasing lock`)
+		}))
+
+		await Promise.all([...tasks1, ...tasksA])
+
+		assert.deepEqual(completed1, ['1', '2', '3'])
+		assert.deepEqual(completedA, ['A', 'B', 'C'])
+
+
+	})
+
+})
