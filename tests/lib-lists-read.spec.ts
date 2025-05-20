e2b2310b8232bc949d28e700e5030bd81d30e761
Ros McMahon <rosmcmahon@gmail.com>
2025-05-20 20:12:45 +0700

    test(lib/lists): test updateTxidsCache

--- tests/lib-lists-read.spec.ts
+++ tests/lib-lists-read.spec.ts
@@ -1,14 +1,14 @@
 import 'dotenv/config'
 import assert from "node:assert/strict"
 import { after, afterEach, beforeEach, describe, it } from 'node:test'
-import { initTxidsCache, initRangesCache } from '../libs/s3-lists/read-lists'
+import { initTxidsCache, initRangesCache, updateTxidsCache } from '../libs/s3-lists/read-lists'
 import { s3DeleteFolder } from '../libs/utils/s3-services'
 import { getLastModified, UpdateItem, updateS3Lists } from '../libs/s3-lists/update-lists'
 
 
 describe('read-lists tests', () => {
 
-	const listname = 'ram-read-test'
+	const listname = 'ram-read-test/'
 	const testRecords1: UpdateItem[] = [
 		{ txid: 'txid01', range: [100, 200] },
 		{ txid: 'txid02', range: [50, 100] },
@@ -19,6 +19,10 @@ describe('read-lists tests', () => {
 		{ txid: 'txid03', range: [150, 250], op: 'remove' }, //there an issue here when chunks overlap
 		{ txid: 'txid05', range: [350, 450] },
 		{ txid: 'txid06', range: [400, 500] },
+	]
+	const testRecords3: UpdateItem[] = [
+		{ txid: 'txid07', range: [500, 600] },
+		{ txid: 'txid08', range: [450, 550] },
 	]
 
 	beforeEach(async () => {
@@ -42,6 +46,30 @@ describe('read-lists tests', () => {
 		// console.log({ lastMod, now, diff: now - lastMod })
 		const maxdiff = 10_000 //ms
 		assert(lastMod < now && lastMod > now - maxdiff, `LastModified should be within ${maxdiff} msecs of current time`)
+	})
+
+	it('should apply updates after init was previously called', async () => {
+		//initial lists created
+		const resTxids = await initTxidsCache(listname)
+		const resRanges = await initRangesCache(listname)
+
+		await updateS3Lists(listname, testRecords3)
+
+		// todo: use `updateTxidsCache` and `updateRangesCache` and test results
+		const resUpdateTxids = await updateTxidsCache({
+			listdir: listname,
+			txidsCache: resTxids.txids,
+			previousModified: resTxids.lastModified
+		})
+		assert.notEqual(resUpdateTxids.lastModified, resTxids.lastModified)
+		assert.equal(resTxids.txids.getTxids().length, 7)
+
+
+
+		//TODO: implement this function & test
+		// updateRangesCache({})
+
+
 	})
 
 })
