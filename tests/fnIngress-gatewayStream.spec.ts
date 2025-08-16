daed07ac5a045a40db898d3f38cad3842f33d6fc
Ros McMahon <rosmcmahon@gmail.com>
2025-08-16 11:21:44 +0700

    test(fnIngress/gatewayStream): misc refactor

--- tests/fnIngress-gatewayStream.spec.ts
+++ tests/fnIngress-gatewayStream.spec.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config'
 import { after, describe, it } from 'node:test'
 import assert from 'node:assert/strict'
-import { gatewayStream, destroyAgent } from '../lambdas/fnIngress/gatewayStream'
+import { gatewayStream, destroyGatewayAgent } from '../lambdas/fnIngress/gatewayStream'
 import { clearTimerHttpApiNodes } from '../libs/utils/update-range-nodes'
 
 
@@ -10,7 +10,7 @@ describe('gatewayStream', () => {
 
 	after(() => {
 		clearTimerHttpApiNodes()
-		destroyAgent()
+		destroyGatewayAgent()
 	})
 
 	it('should return ReadableStream and successfully stream a real transaction', async () => {
