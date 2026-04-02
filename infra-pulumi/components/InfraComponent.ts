0453df7917a810e138e0aba6e4f6e65cd158b922
Ros McMahon <rosmcmahon@gmail.com>
2026-04-03 00:28:20 +0700

    fix(infra/pulumi): remove nats, we will use elasticmq

--- infra-pulumi/components/InfraComponent.ts
+++ infra-pulumi/components/InfraComponent.ts
@@ -15,7 +15,7 @@ export class InfraComponent extends pulumi.ComponentResource {
   public readonly network: docker.Network
   public readonly postgresHost: string
   public readonly minioEndpoint: string
-  public readonly natsUrl: string
+  public readonly sqsEndpoint: string
 
   constructor(name: string, args: InfraComponentArgs, opts?: pulumi.ComponentResourceOptions) {
     super('shepherd:infra:InfraComponent', name, {}, opts)
@@ -23,10 +23,13 @@ export class InfraComponent extends pulumi.ComponentResource {
     const childOpts = { ...opts, parent: this }
     const { network, dbPassword, minioPassword, stackName } = args
     const n = (s: string) => `${s}-shep-${stackName}`
+    if (!args.repoPath) {
+      throw new Error('config.repoPath is required')
+    }
 
     const pgVolume = new docker.Volume('pg-data', { name: n('pg-data') }, childOpts)
     const minioVolume = new docker.Volume('minio-data', { name: n('minio-data') }, childOpts)
-    const natsVolume = new docker.Volume('nats-data', { name: n('nats-data') }, childOpts)
+    const elasticMqVolume = new docker.Volume('elasticmq-data', { name: n('elasticmq-data') }, childOpts)
 
     new docker.Container('postgres', {
       name: n('postgres'),
@@ -55,13 +58,15 @@ export class InfraComponent extends pulumi.ComponentResource {
       restart: 'unless-stopped',
     }, childOpts)
 
-    new docker.Container('nats', {
-      name: n('nats'),
-      image: 'nats:latest',
+    new docker.Container('elasticmq', {
+      name: n('elasticmq'),
+      image: 'softwaremill/elasticmq-native',
       networksAdvanced: [{ name: network.name }],
-      volumes: [{ volumeName: natsVolume.name, containerPath: '/data' }],
-      command: ['-js', '-sd', '/data', '-m', '8222'],
-      ports: [{ internal: 8222, external: 8222 }],
+      volumes: [{ volumeName: elasticMqVolume.name, containerPath: '/data' }],
+      ports: [
+        { internal: 9324, external: 9324 },  // SQS API
+        { internal: 9325, external: 9325 },  // UI
+      ],
       restart: 'unless-stopped',
     }, childOpts)
 
@@ -80,12 +85,12 @@ export class InfraComponent extends pulumi.ComponentResource {
     this.network = network
     this.postgresHost = n('postgres')
     this.minioEndpoint = `http://${n('minio')}:9000`
-    this.natsUrl = `nats://${n('nats')}:4222`
+    this.sqsEndpoint = `http://${n('elasticmq')}:9324`
 
     this.registerOutputs({
       postgresHost: this.postgresHost,
       minioEndpoint: this.minioEndpoint,
-      natsUrl: this.natsUrl,
+      sqsEndpoint: this.sqsEndpoint,
     })
   }
 }
