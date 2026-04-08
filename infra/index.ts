41f6b20e7da164a7e978e190b6a9495d8c259de8
Ros McMahon <rosmcmahon@gmail.com>
2026-04-08 15:58:18 +0700

    feat(infra): export postgresHost, minioEndpoint, sqsEndpoint, networkName as Pulumi stack outputs

--- infra/index.ts
+++ infra/index.ts
@@ -10,4 +10,9 @@ const opts = { provider }
 
 const network = new docker.Network('shepherd', { name: `shepherd-${stackName}` }, opts)
 
-new InfraComponent('infra', { config, network, stackName }, opts)
+const infra = new InfraComponent('infra', { config, network, stackName }, opts)
+
+export const postgresHost = infra.postgresHost
+export const minioEndpoint = infra.minioEndpoint
+export const sqsEndpoint = infra.sqsEndpoint
+export const networkName = network.name
