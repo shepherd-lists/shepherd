9a4cde6eeb1769026f787a7dccbca6b2e7b67e55
Ros McMahon <rosmcmahon@gmail.com>
2026-04-09 12:38:07 +0700

    task(script): update psql.sh script

--- psql.sh
+++ psql.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
-docker exec -it shepherd-pgdb-1 psql -U postgres arblacklist
-docker exec -it shepherd_pgdb_1 psql -U postgres arblacklist
+# docker exec -it shepherd-pgdb-1 psql -U postgres arblacklist
+
+docker exec -it postgres-shep-dev psql -U shepherd arblacklist
 
