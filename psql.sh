2de03aca9f38eebf607b8d85a9ac5b244a6df06e
Ros McMahon <rosmcmahon@gmail.com>
2026-04-16 11:05:19 +0700

    feat: update psql.sh

--- psql.sh
+++ psql.sh
@@ -3,4 +3,5 @@
 # docker exec -it shepherd-pgdb-1 psql -U postgres arblacklist
 
 docker exec -it postgres-shep-dev psql -U shepherd arblacklist
+docker exec -it postgres-shep-staging psql -U shepherd arblacklist
 
