971c6b20fcc346099d67f95896281e2a488999df
Ros McMahon <rosmcmahon@gmail.com>
2026-05-06 01:32:00 +0700

    feat(indexer-next): add missing deps for fnIngress and update imports

--- addons/services/services/indexer-next/src/index-shared/query-processor.ts
+++ addons/services/services/indexer-next/src/index-shared/query-processor.ts
@@ -3,7 +3,7 @@ import { slackLog } from '../../../../libs/utils/slackLog'
 import { arGql } from 'ar-gql'
 import { GQLError } from 'ar-gql/dist/faces'
 import { GQLEdgeInterface } from 'ar-gql/dist/faces'
-import { handler as fnIngressHandler } from '../../../../lambdas/fnIngress/index'
+import { handler as fnIngressHandler } from './fnIngress/index'
 import { TxRecord } from 'shepherd-plugin-interfaces/types'
 import { batchUpsertTxsWithRules } from '../../../../libs/utils/pgClient'
 
