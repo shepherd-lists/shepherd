d69f9ad7926cd0f57bf298a788279135877cc120
Ros McMahon <rosmcmahon@gmail.com>
2026-05-13 18:58:41 +0100

    test(nsfw): remove all the other old tests & related files. start fresh

--- addons/nsfw/tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker compose -p nsfw-test -f docker-compose.test.yml build 
-docker compose -p nsfw-test -f docker-compose.test.yml run shep-runner npx knex migrate:latest
-docker compose -p nsfw-test -f docker-compose.test.yml run test-service npm test
-
-docker compose -p nsfw-test -f docker-compose.test.yml stop
-docker compose -p nsfw-test -f docker-compose.test.yml rm -f
