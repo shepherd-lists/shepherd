fa5354f69d44e0bfe632ba0010bf99dded0d912c
Ros McMahon <rosmcmahon@gmail.com>
2026-06-25 15:38:08 +0100

    fix(nsfw): imports updated

--- addons/nsfw/src/index.ts
+++ addons/nsfw/src/index.ts
@@ -1,10 +1,10 @@
 import { slackLog } from '../../classifier-host/src/utils/slackLog'
 import { runClassifierHost } from '../../classifier-host/src/index'
-import { loadPlugins } from '../../classifier-host/src/0-init/load-plugins'
+import { loadPlugin } from '../../classifier-host/src/0-init/load-plugins'
 
 const main = async () => {
-  const plugins = await loadPlugins('shepherd.config.json')
-  await runClassifierHost(plugins, { addonName: process.env.ADDON_NAME })
+  const plugin = await loadPlugin('shepherd.config.json')
+  await runClassifierHost(plugin, { addonName: process.env.ADDON_NAME })
 }
 
 main().catch(async error => {
